407d0e2f1a0d3ee25ef7dd2ad3328a40b8f2e692
Yasir Gaji <hellopleasantcodes@gmail.com>
2025-01-03 23:07:56 +0100

    Chore: building header and footer components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,27 @@
-import { Box, Flex, Text, Link } from "@chakra-ui/react";
+import { Box, Flex, Text, Link, Image } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Box as="header" bg="brand.500" color="white" py={4}>
+    <Box as="header" bg="brand.background" color="brand.text" py={16}>
       <Flex justify="space-between" align="center" maxW="1200px" mx="auto" px={4}>
-        <Text fontSize="xl" fontWeight="bold">Fragrance Planet</Text>
+        <Image 
+          src="/images/logo.jpeg" 
+          alt="Fragrance Planet Logo" 
+          w="60px" 
+          h="auto"
+          borderRadius= "16px"
+        />
+
+        <Text fontSize="xl" fontWeight="bold">Filters</Text>
+
+        <Text fontSize="xl" fontWeight="bold">Search</Text>
+
         <Flex gap={4}>
-          <Link href="/" _hover={{ textDecoration: "underline" }}>Home</Link>
-          <Link href="/products" _hover={{ textDecoration: "underline" }}>Products</Link>
-          <Link href="/about" _hover={{ textDecoration: "underline" }}>About</Link>
+          <Link href="/" _hover={{ textDecoration: "underline" }}>Sign in</Link>
+          <Link href="/products" _hover={{ textDecoration: "underline" }}>Cart</Link>
+          <Link href="/about" _hover={{ textDecoration: "underline" }}>About us</Link>
         </Flex>
+        
       </Flex>
     </Box>
   );
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -16,7 +16,43 @@ const customConfig = defineConfig({
           400: { value: '#333333' }, //text color
         },
       },
+
+      animations: {
+        shakeX: { value: "shakeX 1s ease-in-out infinite" },
+      },
+
+      spacing: {
+        "128": { value: "32rem" },
+        "144": { value: "36rem" },
+      },
+
+      sizes: {
+        "1/7": { value: "14.285%" },
+        "2/7": { value: "28.571%" },
+        "3/7": { value: "42.857%" },
+      },
     },
+
+    semanticTokens: {
+      colors: {
+        brand: {
+          primary: { value: '{colors.brand.50}' },
+          secondary: { value: '{colors.brand.100}' },
+          background: { value: '{colors.brand.200}' },
+          backgroundHover: { value: '{colors.brand.300}' },
+          text: { value: '{colors.brand.400}' },
+        },
+      },
+    },
+
+    keyframes: {
+      shakeX: {
+        "0%, 100%": { transform: "translateX(-100%)" },
+        "50%": { transform: "translateX(100%)" },
+      },
+    },
+
+  
   },
 });
 
