8fc83e8dd330f05b6139f290d5b221df5fa53117
Yasir Gaji <hellopleasantcodes@gmail.com>
2025-01-06 23:18:19 +0100

    Error: having issues with chakra ui v3

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,21 @@
-'use client'
+'use client';
 
-
-import { Box, Flex, Text, Link, Image, Button, useDisclosure } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Link,
+  Image,
+  Button,
+  useDisclosure,
+  Group,
+  IconButton,
+  Input,
+} from '@chakra-ui/react';
 import { Search, Menu, ShoppingCart } from 'lucide-react';
-
 
 const Header = () => {
   const { isOpen, onToggle } = useDisclosure();
 
-  
   return (
     <Box as="header" bg="brand.background" color="brand.text" py={16}>
       <Flex
@@ -26,26 +33,69 @@ const Header = () => {
           borderRadius="16px"
         />
 
-        <Button
-          leftIcon={<Menu />}
-          variant="outline"
-          onClick={onToggle}
-          className="ml-4 md:ml-0"
-        >
-          Filters
-        </Button>
+        <Group attached className="ml-4 md:ml-0">
+          <IconButton variant="outline" size="sm">
+            <Menu />
+          </IconButton>
+          <Button
+            variant="outline"
+            onClick={onToggle}
+            colorPalette="brand.primary"
+            size="md"
+          >
+            Filters
+          </Button>
+        </Group>
 
-        <Text fontSize="xl" fontWeight="bold">
-          Search
-        </Text>
+        <Flex align="center" gap={9}>
+          <Group attached>
+            <Flex flex={1} maxW="800px" mx={4} className="relative">
+              <Input
+                placeholder="search"
+                size="lg"
+                borderRadius="md"
+                className="w-full pl-10"
+              />
+              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            </Flex>
 
-        <Flex gap={4}>
-          <Link href="/" _hover={{ textDecoration: 'underline' }}>
+            <Button
+              variant="outline"
+              onClick={onToggle}
+              colorPalette="brand.primary"
+              size="md"
+            >
+              Search
+            </Button>
+          </Group>
+
+          <Button
+            onClick={onToggle}
+            colorPalette="brand.secondary"
+            size="md"
+            variant="outline"
+          >
             Sign in
-          </Link>
+          </Button>
+        </Flex>
+
+        <Flex gap={9} justify="space-between" align="center">
           <Link href="/products" _hover={{ textDecoration: 'underline' }}>
-            Cart
+            <Group attached className="ml-4 md:ml-0">
+              <Button
+                variant="outline"
+                onClick={onToggle}
+                colorPalette="brand.primary"
+                size="md"
+              >
+                Cart
+              </Button>
+              <IconButton variant="outline" size="sm">
+                <ShoppingCart />
+              </IconButton>
+            </Group>
           </Link>
+
           <Link href="/about" _hover={{ textDecoration: 'underline' }}>
             About us
           </Link>
