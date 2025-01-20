a0e3d7e9da20ac66f76c974dac79ab71c68c1e55
Yasir Gaji <hellopleasantcodes@gmail.com>
2025-01-20 15:48:16 +0100

    Backend-Feature: configured real time logging using pino

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+
+        return {
+          pinoHttp: {
+            transport: isProduction
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: true,
+                  },
+                },
+            level: isProduction ? 'info' : 'debug',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-  
